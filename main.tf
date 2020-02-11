3e80269eb1e2ab6e5f59f05ce8fe4cabd0d90915
kumarshivam12 <kumarshivam192@gmail.com>
2020-02-11 16:19:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,11 +11,3 @@ resource "aws_vpc" "main" {
     Name = "main"
   }
 }
-resource "aws_subnet" "subnet" {
-  vpc_id     = "${aws_vpc.main.id}"
-  cidr_block = "10.0.1.0/24"
-
-  tags = {
-    Name = "Main"
-  }
-}
