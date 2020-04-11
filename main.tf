d2b2dd047e04339834a908f17c157578365d4a80
Shivam Kumar <kumarshivam192@gmail.com>
2020-04-11 06:02:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,3 +4,15 @@ provider "aws" {
   secret_key = ""
 }
 
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "dedicated"
+
+  tags = {
+    Name = "mainINF"
+  }
+}
+resource "aws_subnet" "new"{
+  cidr_block="10.0.1.0/24"
+  vpc_id     = aws_vpc.main.id
+  }
